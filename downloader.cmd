82ad74f642c18c6cb236f267b61f6671c97a5f91
redactedofficial <93684044+Sebix12@users.noreply.github.com>
2024-03-06 14:29:21 +0100

    Update downloader.cmd
    fixed some stuff for later use, (it was a dedicated project), now its actually usable...

--- downloader.cmd
+++ downloader.cmd
@@ -3,17 +3,16 @@ title downloader
 set link=
 set folder=
 set filetype=
-set filename=%random%
-set /p folder=mappa!
-mkdir %folder%
-cd %folder%
+set /p folder=folder$
+if not exist %folder% mkdir %folder%
+if exist %folder% cd %folder%
 :Download
-set filename=%random%
-set /p link=link!
-set /p filetype=file type!
+set /p link=link$
+set /p filename=name$
+set /p filetype=file type$
 echo Downloading!
-powershell.exe (New-Object Net.WebClient).DownloadFile('%link%', 'feladat_matek_%filename%.%filetype%')
-echo link %link% name felatad_matek_%filename%.%filetype% >> names.txt
+powershell.exe (New-Object Net.WebClient).DownloadFile('%link%', '%filename%.%filetype%')
+echo link %link% name %filename%.%filetype% >> names.txt
 echo Download Done!
 timeout /t 4
 goto :Download
